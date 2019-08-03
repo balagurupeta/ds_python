5b305bc31c2b5630ac5ea89ce41bc18032734055
Your Name <you@example.com>
2019-08-03 19:10:54 +0530

    learning git

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ds_python
+
+All my students are very good they came @7am waited for me 10 mins.
---
+++ spyder1/powercalc.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  3 18:59:11 2019
+
+@author: VijayKumar
+"""
+
+def fun_power(arg1,pow1):
+    return arg1**pow1
